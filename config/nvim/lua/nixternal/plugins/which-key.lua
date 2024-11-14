c6af6f2191bf9cba23440fb45392e159a3bbf23f
Richard A. Johnson <nixternal@gmail.com>
2024-11-14 10:09:32 -0600

    tweaks

--- config/nvim/lua/nixternal/plugins/which-key.lua
+++ config/nvim/lua/nixternal/plugins/which-key.lua
@@ -47,7 +47,7 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>r', group = '[R]ename' },
       { '<leader>s', group = '[S]earch' },
       { '<leader>w', group = '[W]orkspace' },
-      { '<leader>t', group = '[T]oggle' },
+      { '<leader>t', group = '[T]oggle/[T]ab/[T]rouble' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
     },
   },
